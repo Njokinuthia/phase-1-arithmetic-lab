31b0e1536e242d0c89b7fb72f74fd566d95eb7cc
Njoki <kinuthiash@gmail.com>
2022-04-09 14:47:59 +0300

    add mod equation

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 
-// Write your code here
+// Multiply 
 const num1 = 31;
 const num2 = 2;
+const num3 = 16;
+const num4 = 6;
+let multiply = num1*num2;
+console.log(multiply)
 
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-multiply = 62
-console.log(multiply(num1,num2))
+
+// Random
+let random = Math.ceil(Math.random());
+console.log(random);
+
+// mod
+
+let mod = num3 % num4;
+console.log(mod);
